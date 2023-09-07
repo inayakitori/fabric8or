26774d050bda748e55a98face040417327926ed9
Inaya Khurram <s3943498@student.rmit.edu.au>
2023-09-08 01:53:55 +1000

    does not compile

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,77 @@
 use std::any::Any;
 use std::env;
 use std::fmt::format;
+use std::ops::Deref;
+use std::sync::Arc;
 
 use serenity::async_trait;
 use serenity::builder::CreateThread;
-use serenity::model::channel::{ChannelType, Message};
+use serenity::model::channel::{Channel, ChannelType, GuildChannel, Message};
+use serenity::model::channel::Channel::Guild;
 use serenity::model::gateway::Ready;
+use serenity::model::id::{ChannelId, MessageId};
 use serenity::prelude::*;
+use serenity::utils::MessageBuilder;
 
-struct Handler;
+struct Handler{
+    channels: Vec<ChannelInfo>,
+}
+
+
+#[derive(Clone, Default, Debug, Eq, Hash, PartialEq, Deserialize, Serialize)]
+struct Channels {
+    channels: Vec<ChannelInfo>
+}
+
+impl Channels{
+
+    fn contains(&self, channel: ChannelId) -> bool{
+        self.channels.clone().iter().any(|channel_info| channel_info.channel == channel)
+    }
+
+    async fn add_channel(&mut self, channel: ChannelId, message: Message, ctx: &Context) -> Result<(), ()> {
+
+        if let Guild(channel) = channel {
+            if channel.is_text_based(){
+                if let OK()
+
+            }
+        }
+
+        Err(())
+    }
+
+    async fn add_message(&mut self, channel: ChannelId, message: Message, ctx: &Context) -> Result<(),()>{
+        if let Ok(notice_message) = channel.say(ctx, message.content).await {
+            self.channels.push(
+                ChannelInfo{
+                    channel: channel.id,
+                    notice_message: notice_message.id
+                }
+            );
+            Ok(())
+        }
+        Err(())
+    }
+
+}
+
+#[derive(Copy, Clone, Default, Debug, Eq, Hash, PartialEq, Deserialize, Serialize)]
+struct ChannelInfo{
+    channel: ChannelId,
+    notice_message: MessageId,
+}
+
 
 // 326417787968
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+
+        //notic message
+
+        //thread management
         if !msg.author.bot &&
             msg.channel(&ctx.http)
             .await.expect("couldn't get channel")
